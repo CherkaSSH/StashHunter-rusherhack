10e71e9b3eb8092f84009242ee783b0020e2eba8
Churo4ka <72939961+CherkaSSH@users.noreply.github.com>
2024-04-13 17:47:10 +0300

    not it flies with right angles yey!!! render soon(tm)

--- src/main/java/ru/stash/hunter/StashHunterModule.java
+++ src/main/java/ru/stash/hunter/StashHunterModule.java
@@ -1,10 +1,7 @@
 package ru.stash.hunter;
 
-import net.minecraft.commands.arguments.EntityAnchorArgument;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.ChunkPos;
-import net.minecraft.world.phys.Vec3;
 import org.rusherhack.client.api.events.client.EventUpdate;
 import org.rusherhack.client.api.feature.module.ModuleCategory;
 import org.rusherhack.client.api.feature.module.ToggleableModule;
@@ -12,99 +9,112 @@ import org.rusherhack.core.event.subscribe.Subscribe;
 import org.rusherhack.core.setting.BooleanSetting;
 import org.rusherhack.core.setting.NumberSetting;
 
+import java.io.IOException;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.rusherhack.client.api.RusherHackAPI.getNotificationManager;
-import static org.rusherhack.client.api.RusherHackAPI.getRotationManager;
+import static org.rusherhack.client.api.RusherHackAPI.*;
 
 public class StashHunterModule extends ToggleableModule {
     public StashHunterModule(){
         super("StashHunter","Auromatically scans territory", ModuleCategory.PLAYER);
         //settings
-        registerSettings(gap, active, looky, radius);
+        registerSettings(gap, active, radius);
     }
     //setting
-    private final NumberSetting<Integer> gap = new NumberSetting<>("Gap",4,0,10);
-    private final NumberSetting<Integer> radius = new NumberSetting<>("Radius in gaps",4,1,32);
-    private final NumberSetting<Integer> looky = new NumberSetting<>("YLevel",200,-64,320);
+    private final NumberSetting<Integer> gap = new NumberSetting<>("Gap","In blocks",128,0,1024).incremental(16);
+    private final NumberSetting<Integer> radius = new NumberSetting<>("Radius","in gaps",4,1,32);
     private final BooleanSetting active = new BooleanSetting("Active",false);
 
-    private ChunkPos cen;
+    private BlockPos cen;
     private int i;
 
     @Override
     public void onEnable(){
-        if (cen == null){
-            assert mc.player != null;
-            cen = mc.player.chunkPosition();
-            System.out.println(chunks(radius.getValue()));
-        };
+        assert mc.player != null;
+        cen = mc.player.blockPosition();
     }
 
     @Override
     public void onDisable(){
         cen=null;
+        i=1;
     }
     @Subscribe
     public void onUpdate(EventUpdate event){
-        if (mc.player!=null &&active.getValue()){
-            List<ChunkPos> kek = chunks(radius.getValue());
-            lookXZ(kek.get(i));
-            if(dis2chunk(kek.get(i))<=10){
-                i++;
-            }
-            String string = String.valueOf(chunks(radius.getValue()));
-            //getNotificationManager().chat(string);
+        if (cen == null){
+            assert mc.player != null;
+            cen = mc.player.blockPosition();
         }
+        if (mc.player!=null && active.getValue() && cen!=null){
+            List<BlockPos> kek = blocks(radius.getValue()*16);
+            lookXZ(kek.get(i));
+            getNotificationManager().chat("going to "+kek.get(i));
+            if(disxz2block(kek.get(i))<=5&&i!=radius.getValue()*4+2){
+                i++;
+            } else if((i >= kek.size())) {
+                active.setValue(false);
+                getNotificationManager().chat("Done");
+            }
+            // String string = String.valueOf(chunks(radius.getValue()));
+            // getNotificationManager().chat(string);
+        }
+        //if(mc.player.getHealth()<10 ||mc.player.getInventory().getArmor(2).getDamageValue()<20){
+        //    discnnect();
+        //};
     }
 
-    private ArrayList<ChunkPos> chunks(int gap){
-        ArrayList<ChunkPos> chunkies = new ArrayList<>();
-        chunkies.add(new ChunkPos(0,0));
-        //create diagonal chunks
-        for (int i = 1; i <= radius.getValue(); i++) {
-            chunkies.add(new ChunkPos(i,-i));
-            chunkies.add(new ChunkPos(-i,-i));
-            chunkies.add(new ChunkPos(-i,i));
-            chunkies.add(new ChunkPos(i,i));
+    private ArrayList<BlockPos> blocks(int gap){
+        ArrayList<BlockPos> blockies= new ArrayList<>();
+        //doing 1st circle manually cuz why not
+        blockies.add(new BlockPos(0,0,0));
+        blockies.add(new BlockPos(0,0,-1));
+        blockies.add(new BlockPos(-1,0,-1));
+        blockies.add(new BlockPos(-1,0,1));
+        blockies.add(new BlockPos(2,0,1));
+        //everything else is being done automatically
+        for (int i = 2; i <= radius.getValue(); i++){
+            blockies.add(new BlockPos(i,0,-i));
+            blockies.add(new BlockPos(-i,0,-i));
+            blockies.add(new BlockPos(-i,0,i));
+            blockies.add(new BlockPos(i++,0,i));
         }
         //add gaps
-        chunkies.replaceAll(chunkPos -> new ChunkPos(chunkPos.x*gap, chunkPos.z*gap));
-        //move ++ dir by 1 gap
-        for (int i = 0; i < chunkies.size(); i++) {
-            if (i%4==0){
-                chunkies.set(i,new ChunkPos(chunkies.get(i).x+gap,chunkies.get(i).z+gap));
-            }
-        }
-        //apply offset to cen
-        chunkies.replaceAll(chunkPos -> new ChunkPos(chunkPos.x+cen.x,chunkPos.z+cen.z));
-        return chunkies;
+        blockies.replaceAll(blockPos -> new BlockPos(blockPos.getX()*gap,0,blockPos.getZ()*gap));
+        //center to cen
+        blockies.replaceAll(blockPos -> new BlockPos(blockPos.getX()+cen.getX(),0,blockPos.getZ()+cen.getZ()));
+        return blockies;
     }
 
-    private void lookXZ(ChunkPos c) {
+    //boilerplate shit
+    private void lookXZ(BlockPos b) {
         Player p = mc.player;
         int x = (int) p.getX();
         int y = (int) p.getY() + 1;
         int z = (int) p.getZ();
-        double dirx = x - c.x*16;
-        double diry = y - looky.getValue();
-        double dirz = z - c.z*16;
+        double dirx = x - b.getX();
+        double diry = y - b.getY();
+        double dirz = z - b.getZ();
         double len = Math.sqrt(dirx * dirx + diry * diry + dirz * dirz);
         dirx /= len;
-        diry /= len;
         dirz /= len;
-        double pitch = Math.asin(diry);
         double yaw = Math.atan2(dirz, dirx);
         // to degree
-        pitch = pitch * 180.0 / Math.PI;
         yaw = yaw * 180.0 / Math.PI;
         yaw += 90f;
-        p.setXRot((float) pitch);
         p.setYRot((float) yaw);
     }
 
-    private double dis2chunk(ChunkPos c){
-        return mc.player.distanceToSqr( c.x*16,mc.player.getY(),c.z*16);
+    private double disxz2block(BlockPos b){
+        assert mc.player != null;
+        return mc.player.distanceToSqr( b.getX(),mc.player.getY(),b.getZ());
     }
+
+    //private void discnnect(){
+    //    Socket socket = new Socket();
+    //    try {
+    //        socket.close();
+    //    } catch (IOException ignored) {}
+    //}
 }
